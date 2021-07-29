af45960e9af171af9efff17bf3106da68e6af88f
tfurukawa33 <tfurukawa@skywill.jp>
2021-07-29 12:49:57 +0900

    :hammer: DataJpaTestがExtendWithを持つため、除去

--- rest-service/src/test/java/com/example/restservice/repository/ActorRepositoryTest.java
+++ rest-service/src/test/java/com/example/restservice/repository/ActorRepositoryTest.java
@@ -5,16 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+// import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 // import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+// @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(properties = { "spring.datasource.url=jdbc:mysql://db_test/sakila_test" })
