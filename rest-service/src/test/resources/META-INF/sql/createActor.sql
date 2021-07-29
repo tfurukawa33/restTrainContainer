93a8f54d2669516adf1da28fcd4c3f831ef6969b
tfurukawa33 <tfurukawa@skywill.jp>
2021-07-29 12:48:16 +0900

    :memo: テスト用sqlデータを修正

--- rest-service/src/test/resources/META-INF/sql/createActor.sql
+++ rest-service/src/test/resources/META-INF/sql/createActor.sql
@@ -1,3 +1,5 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
 INSERT INTO actor
     (actor_id, first_name, last_name)
 VALUES
@@ -5,14 +7,14 @@ VALUES
     (2, "Bob", "Bob"),
     (3, "Charlie", "Charlie");
 
+INSERT INTO film
+    (film_id, title, language_id)
+VALUES
+    (1, "aaa", 1),
+    (2, "bbb", 1);
+
 INSERT INTO film_actor
     (actor_id, film_id)
 VALUES
     (2, 1),
     (2, 2);
-
-INSERT INTO film
-    (film_id, title)
-VALUES
-    (1, "aaa"),
-    (2, "bbb");
