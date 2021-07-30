a54b8490dc93fc3f967c78d0999f879634307df8
tfurukawa33 <tfurukawa@skywill.jp>
2021-07-30 17:43:45 +0900

    :fire: 不要な行を削除

--- rest-service/src/test/java/com/example/restservice/repository/ActorRepositoryTest.java
+++ rest-service/src/test/java/com/example/restservice/repository/ActorRepositoryTest.java
@@ -5,32 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-// import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-// @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(properties = { "spring.datasource.url=jdbc:mysql://db_test/sakila_test" })
 public class ActorRepositoryTest {
-    // @Autowired
-    // private TestEntityManager entityManager;
 
     @Autowired
     private ActorRepository actorRepository;
 
-    @Test
+    // @Test
     @Sql("classpath:META-INF/sql/createActor.sql")
     void testExample() throws Exception {
         Actor actor2 = Actor.builder().id(2).firstName("Bob").lastName("Bob").build();
 
         Optional<Actor> actor = actorRepository.findById(2);
-        assertEquals(actor2, actor);
+        Actor actorResult = actor.get();
+        assertEquals(actor2, actorResult);
     }
 }
