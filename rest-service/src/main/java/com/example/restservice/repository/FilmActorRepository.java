3b917db1ead6f2851673a1799deee478b806f18f
tfurukawa33 <tfurukawa@skywill.jp>
2021-07-30 17:40:33 +0900

    :fire: 不要なrepoを削除

--- rest-service/src/main/java/com/example/restservice/repository/FilmActorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilmActorRepository extends CrudRepository<FilmActor, Integer> {
-    
-}
--- rest-service/src/main/java/com/example/restservice/repository/FilmCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilmCategoryRepository extends CrudRepository<FilmCategory, Integer> {
-    
-}
